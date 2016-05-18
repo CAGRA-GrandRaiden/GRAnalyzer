1de15198231ebca9c88d475536b9043ab925cae1
Chris Sullivan <sullivan@nscl.msu.edu>
2016-05-18 17:34:23 -0400

    Forgot to add LinkDef.h, bringing it in

---
+++ analyzer/include/LinkDef.h
@@ -0,0 +1,15 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+
+#pragma link C++ class RCNPEvent+;
+#pragma link C++ class DSTMap+;
+#pragma link C++ class std::vector<double>+;
+#pragma link C++ class std::vector<int>+;
+#pragma link C++ class map<string,vector<double>>+;
+#pragma link C++ class map<int,vector<double>>+;
+
+#endif
