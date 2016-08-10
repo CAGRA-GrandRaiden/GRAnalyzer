501d5ddb29c5a37b37d48884e7f4c3f35cde1397
Chris Sullivan <sullivan@nscl.msu.edu>
2016-08-10 09:29:19 -0400

    Added flag to determine whether a GR status message should be posted or not (offline vs gui).

--- analyzer/libGRAnalyzer/include/GRUTinizerInterface.h
+++ analyzer/libGRAnalyzer/include/GRUTinizerInterface.h
@@ -4,25 +4,21 @@
 class RCNPEvent;
 class RCNPInterface;
 
-void StartGRAnalyzer(const char* filename, std::atomic<int>* terminate,std::function<void(RCNPEvent*)> func, bool save_tree=false);
+void StartGRAnalyzer(const char* filename, std::atomic<int>* terminate,std::function<void(RCNPEvent*)> func, bool save_tree=false, bool status=false);
 
 
 class RCNPInterface {
 	bool save_rcnptree;
+	bool show_status;
 public:
 	void SaveTree(bool set) { save_rcnptree = set; }
 	bool SaveTree() { return save_rcnptree; }
-
-
-
+	void ShowStatus(bool set) { show_status = set; }
+	bool ShowStatus() { return show_status; }
 
 
 //// casual user can ignore below code ////////////////////////////////
-        static RCNPInterface& Instance()
-		{
-			static RCNPInterface    instance;
-			return instance;
-		}
+        static RCNPInterface& Instance() { static RCNPInterface instance; return instance; }
 private:
         RCNPInterface() {};
 public:
--- analyzer/libGRAnalyzer/src/GRUTinizerInterface.cc
+++ analyzer/libGRAnalyzer/src/GRUTinizerInterface.cc
@@ -5,10 +5,11 @@
 #include <thread>
 
 std::atomic<int>* TerminateSig;
-void StartGRAnalyzer(const char* filename, std::atomic<int>* terminate,std::function<void(RCNPEvent*)> func, bool save_tree)
+void StartGRAnalyzer(const char* filename, std::atomic<int>* terminate,std::function<void(RCNPEvent*)> func, bool save_tree, bool status)
 {
 	TerminateSig = terminate;
 	RCNPInterface::Instance().SaveTree(save_tree);
+	RCNPInterface::Instance().ShowStatus(status);
 
 	Init(func);
 	start_analyzer(filename);
@@ -20,4 +21,11 @@ int CheckTerminateSignal() {
 
 extern "C" int CheckSignal () {
 	return CheckTerminateSignal();
+}
+int Status() {
+	return int(RCNPInterface::Instance().ShowStatus());
+}
+
+extern "C" int ShowStatus () {
+	return Status();
 }
